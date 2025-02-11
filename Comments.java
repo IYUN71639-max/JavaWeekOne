d92b0fd91fee17901b90d1c2f828c212ff35ab65
IYUN71639-max <iyun@stu.socsd.org>
2025-02-11 10:10:18 -0500

    ----

---
+++ Comments.java
@@ -0,0 +1,19 @@
+/************************************
+*Project: Demo for Comments
+*Programmer: Mr. Data
+*Date: June, 2003
+*Program Name: Comments.java
+*************************************/
+
+import java.io.*;
+public class Comments
+{
+    public static void main (String[]args)
+    {
+        System.out.println("Hi"); // message to user
+        System.out.println("LabOneA"); //assignment
+    }
+    /* if you want to write a detailed comment, and it wraps
+around the screen, use this block style of commenting.
+*/
+}
--- HelloWorld2.java
+++ HelloWorld2.java
@@ -1,6 +1,6 @@
 public class HelloWorld2 {
 
-public static void main(Strings[]args)
+public static void main(String[]args)
 
 {
     System.out.println("Hello World21111");
