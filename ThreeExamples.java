6d636fb8dc1502babdaa0af643d571ea7af982aa
IYUN71639-max <iyun@stu.socsd.org>
2025-02-12 09:58:56 -0500

    Update ThreeExamples.java

--- ThreeExamples.java
+++ ThreeExamples.java
@@ -1,7 +1,7 @@
 public class ThreeExamples {
     public static void main (String[]args)
     {
-        System.out.println("\t\tDog Show Winners\n");
+        System.out.println("\t\tDog Show Winners\n\nPlace:\t1\t2\t3\t4\t5\t6\nDog#:\t76\t32\t18\t47\t11\t29");
     }
     
 }
